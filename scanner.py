495010b4302b23e14abd08c571143c6b96d5cb21
LolArt <artsecurity@protonmail.com>
2019-05-03 18:10:43 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,30 +1,13 @@
+#this program takes some time to scan everything, I recommend you to run it on a vps and wait until its finished.
+#as this program isnt so fast, i would like you to give me some ideas to make it faster, thank you.
 import socket
 import sys
 import os
 from threading import Thread
-#this program takes some time to scan everything, I recommend you to run it on a vps and wait until its finished.
-#as this program isnt so fast, i would like you to give me some ideas to make it faster, thank you.
-def scanback(port):
-    for i in range(127):
-        if i != 10 or i != 127: 
-            for o in range(256):
-                if i != 192 and o != 168 or i != 172 and o < 16 and o > 31 or i != 169 and o != 254: 
-                    for p in range(256): 
-                        for u in range(256):
-                            i-=127
-                            o-=255
-                            p-=255
-                            u-=255
-                            ipb=str(abs(i))+"."+str(abs(o))+"."+str(abs(p))+"."+str(abs(u))
-                            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                            result = sock.connect_ex((ipb, port))
-                            if result == 0:
-                                f.write("[OPEN] "+ipb)
-                                print("[OPEN] "+ipb)
-                            sock.close()
-
-def scan(port):
-    for i in range(127): 
+x=0
+def scan(port, x):
+    z = -1
+    for i in range(254): 
         i += 1 
         if i != 10 or i != 127: 
             for o in range(256): 
@@ -32,13 +15,17 @@ def scan(port):
                     for p in range(256): 
                         for u in range(256): 
                             ip=str(i)+"."+str(o)+"."+str(p)+"."+str(u)
-                            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                            result = sock.connect_ex((ip, port))
-                            if result == 0:
-                                f.write("[OPEN] "+ip)
-                                print("[OPEN] "+ip)
-                            sock.close()
-                        
+                            z+=1
+                            if x == z:
+                                print("[SCANNING] "+ip)
+                                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                                result = sock.connect_ex((ip, port))
+                                if result == 0:
+                                    f.write("[OPEN] "+ip)
+                                    print("[OPEN] "+ip)
+                                else: print("[CLOSED] "+ip)
+                                sock.close()
+                                return
 if __name__ == '__main__':
     if os.getuid() != 0:
         print("                You need to be root!")
@@ -54,7 +41,8 @@ if __name__ == '__main__':
         quit()
     f=open(outpf,"w+")
     print("INTERNET SCANNING STARTED WITH PORT: "+str(port))
-    Thread(target = scan(port)).start()
-    Thread(target = scanback(port)).start()
+    while x < 4228507922:
+        Thread(target = scan(port, x)).start()
+        x+=1
     f.close
     print("All the internet scanned!!")
